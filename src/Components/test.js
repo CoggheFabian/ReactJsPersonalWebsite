3bd5371678d1e5131f4cbc120d25ae958a5968c6
Cogghe_Fabian <CoggheFabian@hotmail.com>
2021-01-18 15:33:30 +0100

    Git init

---
+++ src/Components/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class Test extends React.Component {
+    render() {
+        return <h2>Hi, I am a Car!</h2>;
+    }
+}
+
+export default Test;
