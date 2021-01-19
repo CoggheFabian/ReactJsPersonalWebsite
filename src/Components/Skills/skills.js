dfba0af7bcffe5aa7490b7e00449e57b4dcc28f9
Cogghe_Fabian <CoggheFabian@hotmail.com>
2021-01-19 20:25:02 +0100

    Add skills page

--- src/Components/Skills/skills.js
+++ src/Components/Skills/skills.js
@@ -1,11 +1,35 @@
 import './skills.scss'
 import React from 'react';
 import Header from "../Header/header";
-
+import skills5 from '../../images/skills_5.svg'
 class Skills extends React.Component {
     render() {
         return <>
             <Header/>
+            <section>
+                <h1>Skills</h1>
+                <article>
+                    <h2>Js</h2>
+                    <img src={skills5} alt=""/>
+                    <p>Dit is een korte tekst</p>
+                </article>
+                <article>
+                    <h2>Js</h2>
+                    <img src={skills5} alt=""/>
+                    <p>Dit is een korte tekst</p>
+                </article>
+                <article>
+                    <h2>Js</h2>
+                    <img src={skills5} alt=""/>
+                    <p>Dit is een korte tekst</p>
+                </article>
+                <article>
+                    <h2>Js</h2>
+                    <img src={skills5} alt=""/>
+                    <p>Dit is een korte tekst</p>
+                </article>
+
+            </section>
         </>}
 }
 
