402322a2d38215fd1c928286da9c8362001f7c2a
fabiancogghe <cogghefabian@gmai.com>
2021-03-21 15:51:10 +0100

    Add github links

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -2,13 +2,17 @@ import React from 'react'
 import {Link} from "react-router-dom";
 
 const Projects = ({ projects }) => {
+    async function goToGitHubPage(project){
+        let baseUrl = "https://github.com/CoggheFabian/"
+        window.location = `${baseUrl}${project.name}`
+    }
     return (
         <div>
             {projects.map((project) => (
                 <article>
                     <h2>{project.name}</h2>
                     <p>{project.description}</p>
-                    <Link to={project.html_url}>See on github...</Link>
+                    <a onClick={() => goToGitHubPage(project)}>See on Github ...</a>
                 </article>
             ))}
         </div>
